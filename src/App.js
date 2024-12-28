c6d42d0c93e1968641e3f18a5d6a0d2b482bbbfa
Evangeline Gong <yiwengong2027@u.northwestern.edu>
2024-12-28 14:33:05 -0500

    Cards done

--- src/App.js
+++ src/App.js
@@ -28,40 +28,73 @@ function NavBar() {
   );
 }
 
-function Card() {
+function Pfp(props) {
+
+  return (
+    <img
+      src={props.image}
+      class={`pfp ${props.size === "small" ? "small" : "large"}`}
+      alt="Sample user profile"
+    />
+  )
+}
+
+const savedUsers = [];
+
+function Save(props) {
+  const [saved, setSaved] = useState("no");
+
+  const switchSaved = () => {
+    if (saved === "no") {
+      setSaved("yes");
+    } else {
+      setSaved("no");
+    }
+  }
+
+  useEffect(() => {
+    if (saved === "yes") {
+      savedUsers.push(props.name);
+    } else {
+      savedUsers.forEach((item, index) => {
+        if (item === props.name) {
+          savedUsers.splice(index, 1);
+        }
+      })
+    }
+  }, [saved]);
+
+  return (
+    <div class="save" onClick={switchSaved}>
+      {saved === "yes" ? <FaHeart size={28} /> : <FaRegHeart size={28} />}
+      <h6>{saved === "yes" ? "unsave" : "save"}</h6>
+    </div>
+  )
+}
+
+function Card(props) {
 
   return (
     <div class="card">
-      <div class="usercard">
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/4322/4322991.png"
-          class="pfp"
-          alt="Sample profile of another user"
-        />
-        <h3>Firstname Lastname</h3>
+      <div class="cardlayout">
+        <Pfp image={props.image} size="large" />
+        <h5>{props.name}</h5>
       </div>
-      <h5>A one-sentence note that the user can add to their card.</h5>
-      <div class="usercard">
-        <div class="label">label</div>
-        <div class="label">label</div>
-        <div class="label">label</div>
+      <h6>{props.note}</h6>
+      <div class="cardlayout">
+        <div class="label">{props.label1}</div>
+        <div class="label">{props.label2}</div>
+        <div class="label">{props.label3}</div>
       </div>
-      <div class="usercard">
+      <div class="cardlayout">
+        <Save name={props.name} />
         <div>
-          <img
-            src="https://static.vecteezy.com/system/resources/previews/013/083/604/non_2x/red-heart-icon-illustration-love-signs-in-red-colors-png.png"
-            class="largeicon"
-            alt="Heart icon"
-          />
-          <h6 class="heart">save</h6>
-        </div>
-        <div>
-          <div class="usercard">
-            <div class="smallpfp"></div>
-            <div class="smallpfp"></div>
-            <div class="smallpfp"></div>
+          <div class="cardlayout">
+            {props.mutuals.map(mutual => (
+              <Pfp image={mutual} size="small" />
+            ))}
           </div>
-          <h6 class="mutual">mutual</h6>
+          <h6>{props.mutuals.length === 0 ? "" : "mutual"}</h6>
         </div>
       </div>
     </div>
@@ -81,19 +114,19 @@ function App() {
         <FaSearch size={28} />
         <button>Filter</button>
         <h3>Myfirstname Lastname</h3>
-        <img
-          class="pfp"
-          src="https://cdn-icons-png.flaticon.com/512/3093/3093496.png"
-          alt="Sample profile of the viewer"
-        />
+        <Pfp image="https://cdn-icons-png.flaticon.com/512/3093/3093496.png" size="large" />
       </header>
-      {/* <div class="cards">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+      <div class="cards">
+        <Card image="https://cdn-icons-png.freepik.com/512/147/147141.png" name="Example User1" note="Nice to meet you!" label1="Swimming" label2="Harry Potter" label3="Hiking" mutuals={["https://cdn-icons-png.flaticon.com/512/147/147137.png"]} />
+        <Card image="https://cdn-icons-png.flaticon.com/512/147/147136.png" name="Example User2" note="Click on my card to learn more about who I am!" label1="Digital art" label2="Tea" label3="Food" mutuals={["https://cdn-icons-png.freepik.com/512/147/147139.png", "https://cdn-icons-png.flaticon.com/512/147/147137.png"]} />
+        <Card image="https://cdn-icons-png.freepik.com/512/147/147129.png" name="Example User3" note="Hi!" label1="Basketball" label2="Movies" label3="Photography" mutuals={[]} />
+        <Card image="https://cdn-icons-png.flaticon.com/512/147/147137.png" name="Example User4" note="The weather is nice today!" label1="Jogging" label2="Movies" label3="Shopping" mutuals={["https://cdn-icons-png.freepik.com/512/147/147139.png", "https://cdn-icons-png.flaticon.com/512/147/147136.png", "https://cdn-icons-png.freepik.com/512/147/147141.png"]} />
+        <Card image="https://cdn-icons-png.freepik.com/512/147/147141.png" name="Example User1" note="Nice to meet you!" label1="Swimming" label2="Harry Potter" label3="Hiking" mutuals={["https://cdn-icons-png.flaticon.com/512/147/147137.png"]} />
+        <Card image="https://cdn-icons-png.flaticon.com/512/147/147136.png" name="Example User2" note="Click on my card to learn more about who I am!" label1="Digital art" label2="Tea" label3="Food" mutuals={["https://cdn-icons-png.freepik.com/512/147/147139.png", "https://cdn-icons-png.flaticon.com/512/147/147137.png"]} />
+        <Card image="https://cdn-icons-png.freepik.com/512/147/147129.png" name="Example User3" note="Hi!" label1="Basketball" label2="Movies" label3="Photography" mutuals={[]} />
+        <Card image="https://cdn-icons-png.flaticon.com/512/147/147137.png" name="Example User4" note="The weather is nice today!" label1="Jogging" label2="Movies" label3="Shopping" mutuals={["https://cdn-icons-png.freepik.com/512/147/147139.png", "https://cdn-icons-png.flaticon.com/512/147/147136.png", "https://cdn-icons-png.freepik.com/512/147/147141.png"]} />
       </div>
-      <footer>
+      {/* <footer>
         <h4>Made by Evangeline Gong</h4>
         <img
           src="https://cssdp.org/uploads/2019/08/white-email-icon-png-26.png"
