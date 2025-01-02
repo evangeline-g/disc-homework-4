783e8d7e2ec0f47ad4ad4e1504c6248980be9d09
Evangeline Gong <yiwengong2027@u.northwestern.edu>
2025-01-01 19:28:43 -0500

    added key to .map() and reorganized the Home, Cards, and Card files

---
+++ src/components/Card.js
@@ -0,0 +1,34 @@
+import Pfp from './Pfp.js';
+import Save from "./Save.js";
+
+export default function Card(props) {
+
+    return (
+        <div class="card" key={props.user.id}>
+            <div class="cardlayout">
+                <Pfp image={props.user.profilepicture} size="large" />
+                <h5>{props.user.firstname} {props.user.lastname}</h5>
+            </div>
+            <h6>{props.user.bio}</h6>
+            {/* <div class="cardlayout">
+                            <div class="label">{props.label1}</div>
+                            <div class="label">{props.label2}</div>
+                            <div class="label">{props.label3}</div>
+                        </div> */}
+            <div class="cardlayout">
+                <Save user={props.user} />
+                {/* displays the list of mutuals (friends of the user who are also friends with the viewer)
+                            <div>
+                                <div class="cardlayout">
+                                    {props.mutuals.map(mutual => (
+                                        <Pfp image={mutual} size="small" />
+                                    ))}
+                                </div>
+                                /* but if there's no mutual friend, display nothing
+                                <h6>{props.mutuals.length === 0 ? "" : "mutual"}</h6>
+                            </div> */}
+            </div>
+        </div>
+
+    )
+}
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,34 @@
 import React from "react";
 import { FaTimes, FaSearch, FaEnvelope } from "react-icons/fa";
 import { useState, useEffect } from "react";
-import Cards from './Cards.js';
+import Card from './Card.js';
 import Pfp from "./Pfp.js";
 
 export default function Home() {
+    // the array of users
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-    return (
+    // fetch API whenever only on the first render
+    useEffect(() => {
+        const fetchUsers = async () => {
+            // this is a server that hosts all our data
+            const response = await fetch(
+                "https://disc-assignment-5-users-api.onrender.com/api/users"
+            );
+            const data = await response.json();
+            // when the database responds, we are no longer loading
+            setLoading(false);
+            setUsers(data);
+        };
+
+        fetchUsers();
+    }, []);
+
+    // based on the loading status of the page, show loading or the page content
+    return loading ? (
+        <div className="cards">Loading...</div>
+    ) : (
         <div>
             <header>
                 <div class="searchbar">
@@ -18,7 +40,12 @@ export default function Home() {
                 <h3>Myfirstname Lastname</h3>
                 <Pfp image="https://cdn-icons-png.flaticon.com/512/3093/3093496.png" size="large" />
             </header>
-            <Cards />
+            <div className="cards">
+            {users.map((user) => (
+                <Card user={user} />
+            ))
+            }
+        </div>
             <footer>
                 <h4>Made by Evangeline Gong</h4>
                 <FaEnvelope size={28} color='#3d3d3d' />
